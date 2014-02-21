be96e45ec18ca97a3c0534f18226a667af91b69f
fridde <f@hehl.se>
2014-02-21 11:28:22 +0100

    flow of data

--- ranking.php
+++ ranking.php
@@ -5,12 +5,14 @@ $possibleMainIdArray = Helper::sql_select_columns(ORM::for_table("osdb_ranking")
 echo '<div id="tabs"> ';
 $tabs = array();
 foreach ($possibleMainIdArray as $mainId) {
-        
-    $recentDay = 1000000;
+
     $mainIdName = ORM::for_table('osdb_compilations') -> find_one($mainId) -> Name;
-    $thisTab = array("Title"=>Helper::shorten_names($mainIdName), "Content" => "");
+    $thisTab = array(
+        "Title" => Helper::shorten_names($mainIdName),
+        "Content" => ""
+    );
     $thisTab["Content"] .= '<h1> Based on ' . $mainIdName . '</h1><br>';
-    
+
     $dayArray = Helper::sql_select_columns(ORM::for_table('osdb_ranking') -> distinct() -> order_by_desc('Day') -> select("Day") -> find_array(), "Day");
     foreach ($dayArray as $day) {
         $csvFileName = Helper::shorten_names($mainIdName);
@@ -21,17 +23,19 @@ foreach ($possibleMainIdArray as $mainId) {
         elseif ($day > 800) {
             $timeText = "~ " . floor($day / 365) . " years";
             $csvFileName .= " - " . floor($day / 365) . "y";
-        } else {
+        }
+        else {
             $timeText = $day . " days";
             $csvFileName .= $day . "d";
         }
-       
+
         $compilationIdArray = ORM::for_table('osdb_ranking') -> where("Main_Id", $mainId) -> where("Day", $day) -> find_array();
-        if (count($compilationIdArray) > 1 && ($day < 100 || ($recentDay - $day) > 400)) {
-            $recentDay = $day;
+
+        if (count($compilationIdArray) > 1) {
+
             $compilationIdArray = Helper::create_tournament_ranking($compilationIdArray);
             $graphLinkText = '<a class="tinyLink" target="_blank" href="index.php?page=graphs&compilationId[]=' . $mainId;
-            $csvLinkText = '<a class="tinyLink" target="_blank" href="csv_creater.php?fileName=' . $csvFileName  . '&compilationId[]=' . $mainId;
+            $csvLinkText = '<a class="tinyLink" target="_blank" href="csv_creater.php?fileName=' . $csvFileName . '&compilationId[]=' . $mainId;
             foreach ($compilationIdArray as $compilationId => $wins) {
                 $graphLinkText .= '&compilationId[]=' . $compilationId;
                 $csvLinkText .= '&compilationId[]=' . $compilationId;
@@ -39,9 +43,9 @@ foreach ($possibleMainIdArray as $mainId) {
             $graphLinkText .= '"> [Show in graph]</a>';
             $csvLinkText .= '"> [Download csv]</a>';
 
-           $thisTab["Content"] .=  '<table>
+            $thisTab["Content"] .= '<table>
     <thead>
-    <th>Category: ' .  $timeText . $graphLinkText . $csvLinkText . '</th><th>Wins</th>
+    <th>Category: ' . $timeText . $graphLinkText . $csvLinkText . '</th><th>Wins</th>
     </thead>
     <tbody>
     ';
@@ -58,11 +62,11 @@ foreach ($possibleMainIdArray as $mainId) {
     $tabs[$mainId] = $thisTab;
 }
 echo '<ul>';
-foreach($tabs as $mainId=>$tab){
+foreach ($tabs as $mainId => $tab) {
     echo '<li><a href="#tabs_' . $mainId . '">' . $tab["Title"] . '</a></li>';
 }
 echo '<ul>';
-foreach($tabs as $mainId=>$tab){
+foreach ($tabs as $mainId => $tab) {
     echo '<div id="tabs_' . $mainId . '">' . $tab["Content"] . '</div>';
 }
 echo '</div>';
